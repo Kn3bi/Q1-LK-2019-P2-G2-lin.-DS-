f076b5fdf83dbf17c9ffb7e749a62d6e231bdcc5
Lisa <lisan2205@gmail.com>
2019-12-29 05:58:27 +0100

    Anfang der Refereznen/Attribute + Assoziationen/Aggregationen

--- src/my_project/control/MainController.java
+++ src/my_project/control/MainController.java
@@ -1,4 +1,16 @@
 package my_project.control;
 
 public class MainController {
+
+    private ProgramController programmCon;
+    private SoundController soundCon;
+    private ViewController viewCon;
+    private StrukturController strukturCon;
+
+    public MainController(){
+        viewCon = new ViewController();
+        strukturCon = new StrukturController();
+        soundCon = new SoundController();
+        //programmCon = new ProgramController(viewCon);
+    }
 }
