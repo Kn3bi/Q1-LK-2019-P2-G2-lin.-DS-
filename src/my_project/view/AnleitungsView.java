26443a2c71360840c8a1c69d1a274e5522081bb3
Batmanderes <ugur_akin42@emailn.de>
2020-01-10 11:17:50 +0100

    picture background

--- src/my_project/view/AnleitungsView.java
+++ src/my_project/view/AnleitungsView.java
@@ -3,11 +3,15 @@ package my_project.view;
 import KAGO_framework.model.GraphicalObject;
 import KAGO_framework.view.DrawTool;
 
+import java.awt.image.BufferedImage;
+
 public class AnleitungsView extends GraphicalObject {
+    private BufferedImage feld;
 
     public AnleitungsView(){
         x = 200;
         y = 100;
+
     }
 
     @Override
--- src/my_project/view/SpielView.java
+++ src/my_project/view/SpielView.java
@@ -3,14 +3,20 @@ package my_project.view;
 import KAGO_framework.model.GraphicalObject;
 import KAGO_framework.view.DrawTool;
 
+import java.awt.image.BufferedImage;
+
 public class SpielView extends GraphicalObject {
+    private BufferedImage feld;
 
     public SpielView(){
+        setNewImage("assets/images/field");
+        feld = getMyImage();
 
     }
 
     @Override
     public void draw(DrawTool drawTool) {
+        drawTool.drawImage(feld,0,0);
 
     }
 
